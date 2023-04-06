4a2460a5c11168ea3603928ebd7773c2ed61d186
swu1747 <swu1747@gmail.com>
2023-04-06 00:43:35 -0400

    i

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: false
 }))
+app.get('/', (req, res, next) => {
+    console.log('ahha')
+    res.send('hahahha')
+})
 app.get('/reviews', (req, res, next) => {
     let page = req.query.page ? req.query.page - 1 : 0
     let count = req.query.count ? req.query.count : 5
